a5bd82c21761c62875627a3d143ea97f908f19d3
José Quintino <josequintino@Jose-Quintino.local>
2022-08-23 15:35:36 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'kessefapp',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
