9760dcb503eb6ad4c781101dca5a94cdd3c7d231
Sistema Financeiro - Desenvolvimento <repository.desenvolvimento@gmail.com>
2022-08-30 15:07:12 -0300

    KESSEFAPP0129082022FEATURE
    - Implementar ajustes no gráfico (ChartJS)

--- src/app/page/receita/receita.page.ts
+++ src/app/page/receita/receita.page.ts
@@ -27,28 +27,36 @@ export class ReceitaPage implements OnInit {
         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'November', 'December'],
         datasets: [
           {
-            label: "First dataset",
-            data: [33, 53, 85, 41, 44, 65],
+            label: "",
+            data: [10, 12, 10, 11, 10, 8, 7, 9, 9, 8, 9, 10, 2],
+            tension: 0.6,
+            pointRadius: 0,
             // fill: true,
-            backgroundColor: "rgba(255, 99, 132, 1)",
-            borderColor: 'rgba(255, 99, 132, 0.2)',
-            borderWidth: 2
+            backgroundColor: "#42d487",
+            borderColor: '#30d07c',
+            // borderWidth: 2
           }
         ]
       },
       options: {
         scales: {
           x: {
+            display: false,
             grid: {
-              display: false,
-              drawBorder: false,
-              borderWidth: 3,
-              drawOnChartArea: false,
-              lineWidth: 5,
-              drawTicks: false,
+              // display: false,
+              // drawBorder: false,
+              // borderWidth: 3,
+              // drawOnChartArea: false,
+              // lineWidth: 5,
+              // drawTicks: false,
             },
           },
+          y: {
+            // beginAtZero: true,
+            display: false
+          },
           'y-axis': {
+            display: false,
             grid: {
               display: false,
               drawBorder: false,
